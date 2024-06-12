7c345b83edc87df5e5207dd741919440aecf6a47
DerkNiessink <derk@niessink.com>
2024-06-12 13:09:22 +0200

    simplified job script

--- run.sh
+++ run.sh
@@ -5,41 +5,10 @@
 #SBATCH --tasks-per-node 15
 #SBATCH --mem=60G
 
-
-interrupted=0
-
-# Define a function to run when the INT signal is received
-handle_interrupt() {
-    echo -e "\nDLPOLY was interrupted"
-    kill -KILL "$pid"
-    interrupted=1
-}
-
 cd dl-poly-5.1.0/execute
 
-# Set up the trap
-trap handle_interrupt INT
 echo "Running DLPOLY..."
-./DLPOLY.Z &
-
-pid=$!
-
-# Start the timer
-start=$(date +%s)
-while kill -0 $pid 2>/dev/null; do
-
-    if [ $interrupted -eq 1 ]; then
-        break
-    fi
-
-    now=$(date +%s)
-    elapsed=$((now - start))
-    echo -ne "Elapsed time: $elapsed seconds\033[0K\r"
-    sleep 1
-done
-
-# Reset the trap to the default behavior
-trap - INT
+./DLPOLY.Z
 
 echo -e "\nSaving output files..."
 
